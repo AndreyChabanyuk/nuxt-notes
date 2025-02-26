3e267ebad44511bbf76046f1c941909c54e0aa19
andrey <andrejjchabanjuk@rambler.ru>
2025-02-26 17:27:52 +0300

    login functionality

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,14 @@
-export default defineNuxtRouteMiddleware((event) => {
-    console.log('middleware fire')
-})
+export default defineNuxtRouteMiddleware(async (event) => {
+  if (process.client) return;
+  const { $verifyJwtToken } = useNuxtApp();
+  const jwt = useCookie("NoteNestJWT");
+  if (!jwt.value) {
+    return navigateTo("/register");
+  }
+  console.log("middleware fire");
+  try {
+    await $verifyJwtToken(jwt.value, process.env.JWT_SECRET);
+  } catch (error) {
+    return navigateTo("/register");
+  }
+});
--- pages/login.vue
+++ pages/login.vue
@@ -1,13 +1,93 @@
 <template>
-    <div>
-        
+  <div class="bg-zinc-900 h-screen flex">
+    <div class="bg-black w-[516px] p-12 flex flex-col justify-center">
+      <Logo />
+      <h1 class="text-white font-bold text-lg mt-[44px]">
+        Log in to your account
+      </h1>
+      <p class="text-zinc-300 font-normal mt-0.5">
+        Don't have an account?
+        <nuxt-link class="text-[#FFAC00] underline font-bold" to="/register">
+          Sign up </nuxt-link
+        >for one
+      </p>
+
+      <form @submit.prevent="submit">
+        <div class="mt-[33px]">
+          <label
+            for=""
+            class="text-white text-sm text-zinc-300 block mb-[9.5px]"
+            >Email Address</label
+          >
+          <input
+            v-model="email"
+            placeholder="you@example.com"
+            type=""
+            class="block w-full bg-[#27272A] border border-[#3F3F46] rounded text-white px-4 py-2 placeholder:text-zinc-500 text-sm"
+          />
+        </div>
+        <div class="mt-[33px]">
+          <label
+            for=""
+            class="text-white text-sm text-zinc-300 block mb-[9.5px]"
+            >Password</label
+          >
+          <input
+            placeholder="************"
+            v-model="password"
+            type="password"
+            class="block w-full bg-[#27272A] border border-[#3F3F46] rounded text-white px-4 py-2 placeholder:text-zinc-500 text-sm"
+          />
+        </div>
+        <div>
+          <button
+            class="rounded-full w-full mt-8 bg-[#FFAC00] text-sm font-bold px-4 py-2 flex justify-center items-center space-x-2 text-[#4B360A]"
+          >
+            <span>Log in</span>
+            <ArrowRight class="mt-1.5" />
+          </button>
+        </div>
+      </form>
     </div>
+  </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
+import axios from "axios";
+import Swal from "sweetalert2";
+
+const password = ref("");
+const email = ref("");
+const err = ref("");
+/* const data = ref(null) */
+
+const submit = async () => {
+  console.log("submit");
+  try {
+    const response = await axios.get("/api/user", {
+      email: email.value,
+      password: password.value,
+    });
+    const { isConfirmed } = await Swal.fire({
+      title: "Success",
+      text: "Account created successfully",
+      icon: "success",
+      confirmButtonText: "Close",
+    });
+    if (isConfirmed) {
+      navigateTo("/");
+    }
+  } catch (error) {
+    console.log("Error");
+    console.log(error.response?.data?.message);
+    Swal.fire({
+      title: "Error!",
+      text: error.response?.data?.message,
+      icon: "error",
+      confirmButtonText: "Close",
+    });
+  }
+};
+
 
 </script>
-
-<style>
-    
-</style>
